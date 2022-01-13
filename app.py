cd939616105fac5f6ae77718ba3bc58f192aa8df
swiskacper <swiskacper@gmail.com>
2022-01-13 11:58:31 +0100

    linesWithborder

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 from flask import Flask, request
 import requests, json
-from pandas import array
-from shapely.speedups._speedups import numpy
-
+import dash
 import city
 import weather
 import geocoder
-import overpass
 import overpy
-import array as arr
 import pandas as pd
-import plotly.express as px
 import plotly.graph_objects as go
 import json
 
@@ -18,9 +13,19 @@ app = Flask(__name__)
 
 us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
 
+# pk.eyJ1Ijoic3dpc2thY3BlciIsImEiOiJja3k4bWk2ZzgxNGVjMnBub2R5Y2xrZWN4In0.CaeWG-UQNST74lph1zXXgQ
+#    response = anvil.http.request(f"https://api.mapbox.com/isochrone/v1/mapbox/{profile}/{lnglat.lng},{lnglat.lat}?contours_minutes={contours_minutes}&polygons=true&access_token={self.token}", json=True)
 
-def display_map(coords):
+accesstokenMapbox = 'pk.eyJ1Ijoic3dpc2thY3BlciIsImEiOiJja3k4bWk2ZzgxNGVjMnBub2R5Y2xrZWN4In0.CaeWG-UQNST74lph1zXXgQ'
+
+
+def display_map(coords, startCoord):
     comments, lat, lon = getLatLonAndComments(coords)
+    latHeatMap, lonHeatMap = getHeatCoords("driving", "30", startCoord)
+
+    app = dash.Dash()
+
+
 
     fig = go.Figure(go.Scattermapbox(
         lat=lat,
@@ -31,12 +36,26 @@ def display_map(coords):
             color='#8B0000'
         ),
         text=comments,
+    ),
+    )
+    fig.add_trace(go.Scattermapbox(
+        lat=lonHeatMap,
+        lon=latHeatMap,
+        mode='lines',
+        marker=go.scattermapbox.Marker(
+            size=14,
+            color='#0000FF'
+
+        ),
+        text=" ",
+
     ))
+    scatter=fig.add_scatter(x=lat, y=lon, mode="markers", marker={'size': 15})
 
     fig.update_layout(
         hovermode='closest',
         mapbox=dict(
-            accesstoken='pk.eyJ1Ijoic3dpc2thY3BlciIsImEiOiJja3k4bWk2ZzgxNGVjMnBub2R5Y2xrZWN4In0.CaeWG-UQNST74lph1zXXgQ',
+            accesstoken=accesstokenMapbox,
             bearing=0,
             center=go.layout.mapbox.Center(
                 lat=lat[0],
@@ -44,11 +63,64 @@ def display_map(coords):
             ),
             pitch=0,
             zoom=5
+
         )
     )
 
+
+
+    # fig.update_layout(
+    #     mapbox={
+    #         "style": "open-street-map",
+    #         "zoom": 9,
+    #         "layers": [
+    #             {
+    #                 "below": "traces",
+    #                 "type": "line",
+    #                 "color": "purple",
+    #                 "source": tuplee,
+    #                 "line": {"width": 1.5},
+    #             }
+    #         ],
+    #     },
+    #     margin={"l": 0, "r": 0, "t": 0, "b": 0},
+    # )
+
     fig.show()
     return "SSS"
+
+
+def do_click(trace, points, state):
+    print(points)
+    print(trace)
+    print(state)
+
+#    response = anvil.http.request(f"https://api.mapbox.com/isochrone/v1/mapbox/{profile}/{lnglat.lng},{lnglat.lat}?contours_minutes={contours_minutes}&polygons=true&access_token={self.token}", json=True)
+
+def getHeatCoords(vehicle, minutes, startCoord):
+    prefix = "https://api.mapbox.com/isochrone/v1/mapbox/"
+    postfix = "&polygons=true&access_token="
+    uri = prefix + vehicle + '/' + startCoord[0] + ',' + startCoord[
+        1] + '?' + 'contours_minutes=' + minutes + postfix + accesstokenMapbox
+    result = requests.get(uri).json()
+    features = result['features']
+    # geometry=features['0']['geometry']
+    # for k in range(len(result['features'][0]['geometry']['coordinates'])):
+    print(result['features'][0]['geometry']['coordinates'][0])
+    print(len(result['features'][0]['geometry']['coordinates'][0]))
+    result2 = result['features'][0]['geometry']['coordinates'][0]
+    lat = []
+    lon = []
+    for i in range(len(result2)):
+        lat.append(result2[i][0])
+        lon.append(result2[i][1])
+    return lat, lon
+
+    # print(geometry)
+    # print(len(geometry))
+    # print(geometry['coordinates'])
+    # for i in range(len(geometry)):
+    #     print(geometry[i])
 
 
 def getLatLonAndComments(coords):
@@ -62,7 +134,7 @@ def getLatLonAndComments(coords):
         str = json.loads(json.dumps(coords.ways[i].tags))
         str2 = tuple(coords.ways[i].tags)
         for k in range(len(str2)):
-            comment = comment + str2[k] + ":" + str[str2[k]] + "\n"
+            comment = comment + str2[k] + ":" + str[str2[k]] + "<br>"
         comments.append(comment)
     return comments, lat, lon
 
@@ -117,7 +189,7 @@ def getApiResult(city):
     # print(result.nodes[0])
     # print(result.relations)
     # print(result.nodes[0].tags)
-    display_map(result)
+    display_map(result, [city.lat, city.lon])
 
 
 def makeAQuery(bounds, sunnyAttracions, things):
